b0659d0048aacf72530921257d10104cdbad161d
Cody Olsen <cody.olsen@finn.no>
2020-11-25 19:03:35 +0100

    wireframing

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,7 @@ export default class _DocumentPage extends Document {
             name="viewport"
             content="width=device-width, viewport-fit=cover"
           />
+          <title>React Spring Bottom Sheet</title>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700;900&family=Source+Sans+Pro&display=swap"
