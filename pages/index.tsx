8f8dcadd2cf1e8742964c6b26402b862b783b7e1
Cody Olsen <cody.olsen@finn.no>
2020-11-25 17:32:39 +0100

    working on sexy logo thingy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Nav from '../docs/nav'
+import Hero from '../docs/Hero'
 
 export default function IndexPage() {
   return (
     <div>
+      <Hero />
       <Nav />
       <div className="py-20">
         <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
