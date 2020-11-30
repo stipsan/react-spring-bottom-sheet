1443696a9aad4cc0d01160b97528715248750d98
Cody Olsen <cody.olsen@finn.no>
2020-11-30 09:11:08 +0100

    chore: setup semantic-release

---
+++ release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        tarballDir: 'release',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: 'release/*.tgz',
+      },
+    ],
+    '@semantic-release/git',
+  ],
+  preset: 'angular',
+  branches: ['main'],
+}
