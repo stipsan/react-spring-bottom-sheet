e1a9c7ddcf837654b4a65431b3bbdbde23344670
Cody Olsen <cody.olsen@finn.no>
2020-12-05 04:04:29 +0100

    docs: hover close button

--- docs/fixtures/Container.tsx
+++ docs/fixtures/Container.tsx
@@ -30,7 +30,7 @@ export default function Container({
       )}
     >
       <Link href="/">
-        <a className="absolute left-0 top-0 only-window my-4 mx-8 py-2 px-4 transition-colors focus:duration-0 bg-gray-100 hover:bg-gray-200 active:bg-gray-300 text-gray-600 hover:text-gray-700 active:text-gray-800 text-xs rounded-md focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-white focus-visible:ring-gray-400">
+        <a className="z-10 absolute left-0 top-0 only-window my-4 mx-8 py-2 px-4 transition-colors focus:duration-0 bg-gray-100 hover:bg-gray-200 active:bg-gray-300 text-gray-600 hover:text-gray-700 active:text-gray-800 text-xs rounded-md focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-white focus-visible:ring-gray-400">
           Close example
         </a>
       </Link>
